725fe26993cd14fde60a517069611cf45150c2b2
kokofan <kokofan@kokos-MacBook-Pro.local>
2020-09-22 19:23:06 -0500

    timing and print message

--- src/MPR_close.c
+++ src/MPR_close.c
@@ -32,7 +32,7 @@ MPR_return_code MPR_flush(MPR_file file)
 	}
 	file->time->total_end = MPI_Wtime(); /* the end time for the program */
 
-//	MPR_timing_output(file, lvi, (lvi + lvc));
+	MPR_timing_output(file, lvi, (lvi + lvc));
 
 	return MPR_success;
 }
@@ -73,11 +73,23 @@ static void MPR_timing_output(MPR_file file, int svi, int evi)
 	double max_total_time = 0;
 	MPI_Allreduce(&total_time, &max_total_time, 1, MPI_DOUBLE, MPI_MAX, file->comm->simulation_comm);
 
+	if (rank == 0)
+	{
+		char mode[100];
+		if (file->mpr->aggregation_mode == 0)
+			sprintf(mode, "%s", "Fixed-size");
+		else if (file->mpr->aggregation_mode == 1)
+			sprintf(mode, "%s", "Fixed-patch");
+		else
+			sprintf(mode, "%s", "Default");
+		printf("Current aggregation mode is %s mode\n", mode);
+	}
+
 	if (MODE == MPR_RAW_IO)
 	{
 		if (file->mpr->is_aggregator == 1)
 			fprintf(stderr,"AGG_%d: [%f] >= [rst %f agg %f w_dd %f w_meda %f]\n", rank, total_time, rst_time, agg_time, wrt_data_time, wrt_metadata_time);
 		if (total_time == max_total_time)
-			fprintf(stderr, "MAX %d: [%f] >= [rst %f agg %f w_dd %f w_meda %f]\n", rank, total_time, rst_time, agg_time, wrt_data_time, wrt_metadata_time);
+			fprintf(stderr, "MAX_%d: [%f] >= [rst %f agg %f w_dd %f w_meda %f]\n", rank, total_time, rst_time, agg_time, wrt_data_time, wrt_metadata_time);
 	}
 }
