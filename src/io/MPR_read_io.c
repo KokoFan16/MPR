d7f449fe85e3e7f6dc6eceae60a2b725b997de11
kokofan <kokofan@kokos-MacBook-Pro.local>
2020-10-24 22:30:55 -0500

    fix a bug of initial patch array

--- src/io/MPR_read_io.c
+++ src/io/MPR_read_io.c
@@ -17,6 +17,13 @@ MPR_return_code MPR_raw_read(MPR_file file, int svi)
 		return MPR_err_file;
 	}
 
+	/* get local box for each process */
+	if (MPR_get_local_read_box(file, svi) != MPR_success)
+	{
+		fprintf(stderr, "File %s Line %d\n", __FILE__, __LINE__);
+		return MPR_err_file;
+	}
+
 	return MPR_success;
 }
 
@@ -66,11 +73,17 @@ MPR_return_code MPR_read_data(MPR_file file, int svi)
 	int file_num = file->mpr->open_file_num; /* the number of files need to be opened for each process */
 	int bytes = file->variable[svi]->vps * file->variable[svi]->bpv/8; /* bytes per data */
 
-	MPR_check_required_patches(file, svi); /* convert read box to patch ids */
+	int ret = 0;
+
+	ret = MPR_check_required_patches(file, svi); /* convert read box to patch ids */
+	if (ret != MPR_success)
+	{
+		fprintf(stderr, "File %s Line %d\n", __FILE__, __LINE__);
+		return MPR_err_file;
+	}
 
 	MPR_local_patch local_patch = file->variable[svi]->local_patch; /* Local patch pointer */
-	int required_patch_count = local_patch->agg_patch_count;
-//	local_patch->patch = malloc(sizeof(MPR_patch*) * required_patch_count); /* Local patch array per variable */
+	int required_patch_count = local_patch->patch_count;
 
 	int* patches_offset = malloc(file->mpr->total_patches_num * sizeof(int)); /* patch offset array */
 	int* patches_size = malloc(file->mpr->total_patches_num * sizeof(int)); /* patch size array */
@@ -79,7 +92,12 @@ MPR_return_code MPR_read_data(MPR_file file, int svi)
 	for (int i = 0; i < file_num; i++)
 	{
 		sprintf(file_name, "%s/time%09d/%d", file->mpr->filename, file->mpr->current_time_step, file->mpr->open_file_ids[i]);
-		MPR_file_related_metadata_parse(file_name, file, svi, patches_offset, patches_size, patches_subbands);  /* parse file related meta-data */
+		ret = MPR_file_related_metadata_parse(file_name, file, svi, patches_offset, patches_size, patches_subbands);  /* parse file related meta-data */
+		if (ret != MPR_success)
+		{
+			fprintf(stderr, "File %s Line %d\n", __FILE__, __LINE__);
+			return MPR_err_file;
+		}
 
 		FILE * fp = fopen(file_name, "r"); /* Open bounding box meta-data file */
 		if (fp == NULL)
@@ -93,8 +111,6 @@ MPR_return_code MPR_read_data(MPR_file file, int svi)
 			int global_id = local_patch->agg_patch_id_array[p];
 			if (patches_offset[global_id] != -1)
 			{
-//				printf("%d: %dx%dx%d, %dx%dx%d\n", global_id, local_patch->patch[p]->offset[0], local_patch->patch[p]->offset[1], local_patch->patch[p]->offset[2],
-//						local_patch->patch[p]->size[0], local_patch->patch[p]->size[1], local_patch->patch[p]->size[2]);
 				local_patch->patch[p]->patch_buffer_size = patches_size[global_id];
 				local_patch->patch[p]->buffer = malloc(local_patch->patch[p]->patch_buffer_size);
 
@@ -110,12 +126,15 @@ MPR_return_code MPR_read_data(MPR_file file, int svi)
 		fclose(fp); /* close file */
 	}
 
-	for (int i = 0; i < 512; i++)
-	{
-		float a;
-		memcpy(&a, &local_patch->patch[0]->buffer[i*sizeof(float)], sizeof(float));
-		printf("%f\n", a);
-	}
+//	if ( file->comm->simulation_rank == 3)
+//	{
+//		for (int i = 0; i < 512; i++)
+//		{
+//			float a;
+//			memcpy(&a, &local_patch->patch[0]->buffer[i*sizeof(float)], sizeof(float));
+//			printf("%f\n", a);
+//		}
+//	}
 
 	free(patches_offset);
 	free(patches_size);
@@ -135,7 +154,7 @@ MPR_return_code MPR_check_required_patches(MPR_file file, int svi)
 	for (int i = 0; i < MPR_MAX_DIMENSIONS; i++) /* The current offset plus the global offset to read */
 	{
 		local_offset_xyz[i] = (file->mpr->local_offset[i] + file->mpr->global_offset[i]);
-		local_end_xyz[i] = ceil((local_offset_xyz[i] + file->mpr->local_box[i]) / (float)file->mpr->patch_box[i]);
+		local_end_xyz[i] = ceil((float)(local_offset_xyz[i] + file->mpr->local_box[i]) / file->mpr->patch_box[i]);
 		local_offset_xyz[i] /= file->mpr->patch_box[i];
 		patch_count_xyz[i] = ceil((float)file->mpr->origin_global_box[i] / file->mpr->patch_box[i]);
 	}
@@ -149,18 +168,31 @@ MPR_return_code MPR_check_required_patches(MPR_file file, int svi)
 			for (int i = local_offset_xyz[0]; i < local_end_xyz[0]; i++)
 			{
 				int patch_id = k * patch_count_xyz[0] * patch_count_xyz[1] + j * patch_count_xyz[0] + i;
-				local_patch->patch[patch_id] = (MPR_patch)malloc(sizeof(*local_patch->patch[patch_id]));/* Initialize patch pointer */
-				local_patch->patch[patch_id]->global_id = patch_id;
-				local_patch->patch[patch_id]->offset[0] = i * file->mpr->patch_box[0];
-				local_patch->patch[patch_id]->offset[1] = j * file->mpr->patch_box[1];
-				local_patch->patch[patch_id]->offset[2] = k * file->mpr->patch_box[2];
-				memcpy(local_patch->patch[patch_id]->size, file->mpr->patch_box, MPR_MAX_DIMENSIONS*sizeof(int));
-				local_patch->agg_patch_id_array[required_patch_count++] = patch_id;
+				local_patch->patch[required_patch_count] = (MPR_patch)malloc(sizeof(*local_patch->patch[required_patch_count]));/* Initialize patch pointer */
+				local_patch->patch[required_patch_count]->global_id = patch_id;
+				local_patch->patch[required_patch_count]->offset[0] = i * file->mpr->patch_box[0];
+				local_patch->patch[required_patch_count]->offset[1] = j * file->mpr->patch_box[1];
+				local_patch->patch[required_patch_count]->offset[2] = k * file->mpr->patch_box[2];
+				memcpy(local_patch->patch[required_patch_count]->size, file->mpr->patch_box, MPR_MAX_DIMENSIONS*sizeof(int));
+				local_patch->agg_patch_id_array[required_patch_count] = patch_id;
+				required_patch_count++;
 			}
 		}
 	}
-	local_patch->agg_patch_count = required_patch_count;
+	local_patch->patch_count = required_patch_count;
 	local_patch->patch = realloc(local_patch->patch, sizeof(MPR_patch*) * required_patch_count);
 
+	return MPR_success;
+}
+
+
+MPR_return_code MPR_get_local_read_box(MPR_file file, int svi)
+{
+	MPR_local_patch local_patch = file->variable[svi]->local_patch;
+//	for (int i = 0; i < local_patch->patch_count; i++)
+//	{
+//
+//	}
+//	printf("%d %dx%dx%d\n", file->comm->simulation_rank, file->mpr->local_box[0], file->mpr->local_box[1], file->mpr->local_box[2]);
 	return MPR_success;
 }
--- src/io/MPR_read_io.h
+++ src/io/MPR_read_io.h
@@ -20,4 +20,6 @@ MPR_return_code MPR_read_data(MPR_file file, int svi);
 
 MPR_return_code MPR_check_required_patches(MPR_file file, int svi);
 
+MPR_return_code MPR_get_local_read_box(MPR_file file, int svi);
+
 #endif /* SRC_IO_MPR_READ_IO_H_ */
