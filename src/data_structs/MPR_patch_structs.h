344eb230b12e273ceef8127918d5c1461d5f6fe0
kokofan <kokofan@kokos-MacBook-Pro.local>
2021-01-07 23:44:34 -0600

    remove useless variables

--- src/data_structs/MPR_patch_structs.h
+++ src/data_structs/MPR_patch_structs.h
@@ -12,19 +12,13 @@ struct mpr_patch_struct
 {
 	int global_id;                                  /* global id for patch */
 
-	int owned_rank;                                 /* the rank id which owns this patch */
-
 	unsigned char* buffer;                          /* data buffer of a patch */
 
 	int patch_buffer_size;                          /* the size of the buffer */
 
-	int patch_size[MPR_MAX_DIMENSIONS];             /* restructured grid size */
 
 	int offset[MPR_MAX_DIMENSIONS];                 /* offset of patch in the 3D global space */
 	int size[MPR_MAX_DIMENSIONS];                   /* size (extents) in each of the dimensions for patch */
-
-	int physical_offset[MPR_MAX_DIMENSIONS];        /* the physical offset of a patch in 3D local space */
-	int physical_size[MPR_MAX_DIMENSIONS];          /* the physical size in each dimensions for patch */
 
 	int* subbands_comp_size;                        /* the compressed size of each sub-band */
 };
