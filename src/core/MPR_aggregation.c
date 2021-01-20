b1259f8c9692fb6b663017cdb59e75e7fcecbf25
kokofan <kokofan@kokos-MacBook-Pro.local>
2021-01-20 09:55:22 -0600

    fix a bug

--- src/core/MPR_aggregation.c
+++ src/core/MPR_aggregation.c
@@ -217,7 +217,7 @@ MPR_return_code MPR_aggregation_perform(MPR_file file, int svi, int evi)
 		file->mpr->out_file_num = cur_agg_count + 1;
 
 		int agg_ranks[file->mpr->out_file_num]; /* AGG Array */
-		int gap = ceil(proc_num / (float)file->mpr->out_file_num);
+		int gap = proc_num / file->mpr->out_file_num;
 
 		int cagg = 0;
 		for (int i = 0; i < proc_num; i+= gap)
