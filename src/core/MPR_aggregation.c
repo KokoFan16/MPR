ef9baf2a3dac1be07235790e8ff7e35732076206
kokofan <kokofan@kokos-MacBook-Pro.local>
2021-07-07 22:12:39 -0500

    git comment print message

--- src/core/MPR_aggregation.c
+++ src/core/MPR_aggregation.c
@@ -451,8 +451,8 @@ MPR_return_code MPR_aggregation_perform(MPR_file file, int svi, int evi)
 				local_patch->bounding_box[i + MPR_MAX_DIMENSIONS] = file->mpr->local_box[i] + 1;
 			}
 
-			printf("Aggregation %d: [ gather %f assign %f flat %f cre_comm %f pre %f comm %f ] \n", rank, gather_time, assign_time, flat_time, cre_comm_time, pre_time,
-					comm_time);
+//			printf("Aggregation %d: [ gather %f assign %f flat %f cre_comm %f pre %f comm %f ] \n", rank, gather_time, assign_time, flat_time, cre_comm_time, pre_time,
+//					comm_time);
 		}
 	}
 	return MPR_success;
