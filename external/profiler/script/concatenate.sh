5b7ae0b0fd5097a5b36be28953b3034100b3d05d
kokofan <koko14fan@gmail.com>
2022-09-29 23:01:30 -0400

    update script

--- external/profiler/script/concatenate.sh
+++ external/profiler/script/concatenate.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-while getopts f: flag
+while getopts f:p: flag
 do
     case "${flag}" in
         f) filename=${OPTARG};;
+        p) count=${OPTARG};;
     esac
 done
 
 nfile=$(ls -l . | grep $filename\_.*.csv | wc -l);
 echo "File name: $filename, $nfile";
 
-if [[ $nfile -gt 0 ]] 
+if [[ $nfile -eq $count ]] 
 then
     cat $filename\_*csv > $filename.csv;
     rm -rf $filename\_*csv 
 else
-    echo "merge nothing"; 
+    echo "Didn't find $count files"; 
 fi
 
